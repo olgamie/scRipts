aa020d363c350a028b15c3c80536072a46b41b09
olgamie <olga.mierzwa@gmail.com>
2015-04-06 12:39:34 +0200

    homework1

--- R course/homework1_template.R
+++ R course/homework1_template.R
@@ -30,7 +30,7 @@
 #
 # RETURN VALUE
 # winsor returns a winsorized mean if the values in x is computed as numeric vector of length one. If x contains NA
-# they are put at the begining of the vector when sorting x values in ascending order.
+# they are removed.
 
 ## ---- Function ----
 
@@ -41,12 +41,12 @@ is.natural<- function(x, tol = .Machine$double.eps^0.5){
 # function to calculate winsorized mean
 winsor <- function(x, k){
   #check conditions
-  stopifnot(is.vector(x), is.numeric(k)) 
+  stopifnot(is.finite(x), is.numeric(k)) 
   stopifnot(is.natural(k))
   len <- length(x)
   stopifnot(k <= ((len-1)/2))
   #sort
-  x <- sort(x, na.last = F)
+  x <- sort(x)
   smallest <- x[k+1]
   largest <- x[len-k]
   begin <- rep(smallest,k)
@@ -60,30 +60,15 @@ winsor <- function(x, k){
 library('testthat')
 expect_that(winsor(1:10,2), is_a("numeric"))
 expect_equal(winsor(1:10,3), 5.5)
-expect_error(winsor(1:10,1))
+expect_error(winsor(1:10,0))
 expect_error(winsor(1,0))
 expect_error(winsor(rep(1,100),0.8))
 expect_error(winsor(rep(1,100),-1))
 expect_error(winsor(rep(1,100),50))
 expect_error(winsor(rep(1,100)))
 expect_error(winsor(c(),1))
-expect_identical(winsor(c(1, 2, 3, Inf, Inf), 1), Inf)
-expect_identical(winsor(c(NA, 2, NA, Inf, Inf), 1), as.numeric(NA))
-expect_that(winsor(c(NA, 2, NA, Inf, Inf), 1), is_a("numeric"))
-
-# examples
-do.call(winsor, list(x = rep(1,100), k = 3))
-do.call(winsor, list(x = rnorm(10), k = 3))
-do.call(winsor, list(x = c(NA,1,2), k = 3))
-do.call(winsor, list(x = c(NA, NA, NA, NA), k = 0.5))
-do.call(winsor, list(x = seq(1,100,3), k = 10))
-do.call(winsor, list(x = rnorm(1000000), k = -6))
-do.call(winsor, list(x = rnorm(1000000), k = 6))
-do.call(winsor, list(x = rnorm(1000000), k = 'one'))
-do.call(winsor, list(x = c(1, 2, 3, Inf, Inf), k = 1))
-do.call(winsor, list(x = c(1, NaN, NaN, Inf, Inf), k = 1))
-do.call(winsor, list(x = c(NA, 2, NA, Inf, Inf), k = 1))
-do.call(winsor, list(x = 1, k = 0))
+expect_error(winsor(c(1, 2, 3, Inf, Inf), 1))
+expect_error(winsor(c(NA, 2, NA, Inf, Inf), 1))
 
 ## ------------------------ Exercise 01.02 ----------------------------
 
@@ -101,9 +86,11 @@ do.call(winsor, list(x = 1, k = 0))
 
 ## ---- Function ----
 replacena <- function(x){
+  #check conditions
   stopifnot(is.numeric(x))
+  stopifnot(!is.na(x[1]), !is.na(x[length(x)]))  
   pos <- which(is.na(x)==T)
-  res <- sapply(pos, function(i) (x[i-1]+x[i+1])/2)
+  res <- sapply(pos, function(i) (x[i-1]+x[i+1])/2)  
   stopifnot(length(which(is.na(res)))==0)
   x[pos] <- res
   as.numeric(x)
@@ -112,7 +99,7 @@ replacena <- function(x){
 ## ---- Examples ----
 # tests:
 library(testthat)
-expect_identical(replacena(numeric(0)), numeric(0))
+expect_error(replacena(numeric(0)))
 expect_error(replacena(mean))
 expect_equivalent(replacena(c(-1,NA,3)), c(-1,1,3))
 expect_error(replacena(c(-1,2,NA)))
@@ -126,10 +113,14 @@ test4 <- rep(NA, 10)
 expect_error(replacena(test4))
 test5 <- rnorm(100)
 expect_identical(replacena(test5),test5)
-# examples:
 vec <- c(NA, NA, 2, 3)
-replacena(vec) # some random data
-
+expect_error(replacena(vec))
+vec2 <- c(NA, 1, 2, 3)
+expect_error(replacena(vec2))
+vec3 <- c(1, 1, 2, NA)
+expect_error(replacena(vec3))
+vec4 <- c(NA, 1, 1, 2, NA)
+expect_error(replacena(vec4))
 
 ## ------------------------ Exercise 01.03 ----------------------------
 
@@ -158,41 +149,21 @@ neighboreq <- function(x){
 #unit tests
 test1 <- rep(c(TRUE, FALSE), 4)
 expect_identical(neighboreq(test1),integer(0))
-
 test2 <- rep(c(TRUE, TRUE), 4)
 expect_identical(neighboreq(test2), c(1:7))
-
 test3 <- c(NA, NA, NA)
 expect_identical(neighboreq(test3), integer(0))
-
 test4 <- c(NA, NA, 2,  2)
 expect_equal(neighboreq(test4), 3)
-
 test5 <- list(a = 1)
 expect_error(neighboreq(test5))
-
 expect_error(neighboreq(mean))
-
 test6 <- as.data.frame(matrix(1, 2, 2))
 expect_error(neighboreq(test6))
-
 test7 <- c(Inf, Inf)
 expect_equal(neighboreq(test7), 1)
-
 expect_that(neighboreq(test7), is_a("integer"))
 expect_that(neighboreq(test2), is_a("integer"))
-
-#examples
-neighboreq(c(Inf, Inf, -Inf))
-neighboreq(c('apple', 'car', 'car', 'car', 'apple'))
-neighboreq(rep(c(T,F,T), 3))
-neighboreq(rep(c(2L,1L,1L), 3))
-neighboreq(rep(c(1,2,2,3,1), 3))
-neighboreq(rep(c(1,NA,NA,3,1), 3))
-neighboreq(matrix(1,2,3))
-neighboreq(list(a = 1, b =3, c ="car"))
-neighboreq(rnorm(100))
-neighboreq(sample(c(0,1), 100, replace = TRUE))
 
 ## ------------------------ Exercise 01.04 ----------------------------
 
@@ -210,7 +181,8 @@ neighboreq(sample(c(0,1), 100, replace = TRUE))
 ## ---- Function ----
 
 sample2 <- function(x, k){
-  stopifnot(k>=0, is.atomic(x), length(x) >= 1L)
+  stopifnot(k>=0, is.atomic(x))
+  stopifnot(!(length(x) == 0 & k > 0))
   len = length(x)
   indice <- round(runif(k, 1, len))
   x[indice]
@@ -218,6 +190,10 @@ sample2 <- function(x, k){
 
 ## ---- Examples ----
 #unit test
+expect_error(sample2(numeric(0),1))
+expect_equal(sample2(numeric(0),0), numeric(0))
+expect_equal(sample2(c(),0), NULL)
+expect_equal(sample2(integer(0),0), integer(0))
 vec1 <- rep(c(1,2,3), 10)
 expect_error(sample2(vec1))
 expect_error(sample2(mean, 1))
@@ -242,28 +218,42 @@ expect_that(sample2(vec6, 10), is_a('integer'))
 ## ------------------------ Exercise 01.05 ----------------------------
 
 ## ---- Documentation ----
-
-# ... TO DO ...
-
-
+# DESCRIPTION
+# merge_string_list() joins each string in a given list of character
+# vectors x into a single string.
+#
+# ARGUMENTS
+# x - list of character vectors
+# sep - a character string to seperate joined strings. empty by default
+#
+# RETURN VALUE
+# character string of joined strings from list
 
 ## ---- Function ----
 
-# ... TO DO ...
-
-
+merge_string_list <- function(x, sep = ''){
+  stopifnot(is.list(x), is.character(sep))
+  stopifnot(sapply(x, function(i){is.character(i)}))
+  paste(unlist(x), collapse = sep)
+}
 
 ## ---- Examples ----
-
-# ... TO DO ...
-
-
-
-
-
-
-
-
+x <- list(l1=c("a","b","c"), l2=c("b","d"))
+x2 <- list(l1="c", l2=c("b","d"))
+x3 <- list(l1=c(1,2), l2=c("b","d"))
+x4 <- list(l1=c("car", "apple"), l2=c(1,2,3))
+x5 <- list(l1=c(NA, "apple"), l2=c("1","2","3"))
+expect_identical(merge_string_list(x2, sep=""), "cbd")
+expect_error(merge_string_list(x2, 1))
+expect_error(merge_string_list(x3))
+expect_error(merge_string_list(x4))
+expect_error(merge_string_list(mean))
+expect_error(merge_string_list(c(1,2,3)))
+expect_that(merge_string_list(x2, sep=""), is_a('character'))
+expect_identical(merge_string_list(x5, sep=""), "NAapple123")
+expect_error(merge_string_list(list()))
+expect_error(merge_string_list(list()))
+expect_identical(merge_string_list(list(a=character(0))), "")
 
 ## ------------------------ Exercise 01.06 ----------------------------
 
